cc52cdfe2ac2f2e36a692c1063eec9233b602270
Joseph Lee <Joseph.Lee2@ibm.com>
2024-06-21 12:41:00 +0800

    Website basic placeholder page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Container, Typography, Box } from '@mui/material'
+import ConstructionRoundedIcon from '@mui/icons-material/ConstructionRounded';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <Box display="flex" alignItems={"center"} justifyContent={"center"} sx={{ width: "100%", height: "100%", backgroundColor: "#242424", color: "white" }}>
+            <Box textAlign={"center"}>
+                <ConstructionRoundedIcon sx={{height: "5rem", width: "5rem"}}/>
+                <Typography variant='h2' component="h1">Under Construction</Typography>
+                <Typography variant='h5' component="h2">cat2.link Website Replacement</Typography>
+            </Box>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        </Box>
+    )
 }
 
 export default App
